a36c073e2326e8f1b2f1d692b146d9d230348d1f
Omartwafshah <omartwafshah3@gmail.com>
2022-08-16 15:46:38 +0300

    V1.12

--- src/main/java/com/example/BIWorld/requests/FilterJobs.java
+++ src/main/java/com/example/BIWorld/requests/FilterJobs.java
@@ -1,4 +1,9 @@
 package com.example.BIWorld.requests;
 
 public class FilterJobs {
+    private String filed ;
+    private String degree;
+    private String gender ;
+    private String city ;
+
 }
